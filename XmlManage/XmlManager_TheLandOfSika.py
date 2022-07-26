b01a7bd719dff5ad373a100fe45d6a77c6aa72d5
thomas_soutif <thomas.soutif@nuabee.fr>
2022-07-26 18:43:14 +0200

    Ajout d'une fonction get_all_translate_nodes() et refactorisation du code

--- XmlManage/XmlManager_TheLandOfSika.py
+++ XmlManage/XmlManager_TheLandOfSika.py
@@ -1,5 +1,6 @@
 from Exceptions.XmlManage.Exceptions_XmlManage import CheckFileXMLException
 from XmlManage.XmlManager import XmlManager
+import xml.etree.ElementTree as ET
 
 
 class XmlManagerTheLandOfSika(XmlManager):
@@ -9,7 +10,11 @@ class XmlManagerTheLandOfSika(XmlManager):
         XmlManager.__init__(self, file_path)
         self.check_xml_file()
 
-
     def check_xml_file(self):
-        if not self.get_nodes("strings"):
-            raise CheckFileXMLException(f"Le fichier xml {self.file_path} n'est pas reconnue par le manager {self.manager_name}", file_path=self.file_path, manager_name=self.manager_name)
+        if not self.get_all_translate_nodes():
+            raise CheckFileXMLException(
+                f"Le fichier xml {self.file_path} n'est pas reconnue par le manager {self.manager_name}",
+                file_path=self.file_path, manager_name=self.manager_name)
+
+    def get_all_translate_nodes(self) -> ET.Element:
+        return self.get_nodes("strings")
