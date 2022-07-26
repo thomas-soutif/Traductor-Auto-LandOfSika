0debef83335f9a1066981bcaa3c9667855f8f239
thomas_soutif <thomas.soutif@nuabee.fr>
2022-07-26 18:32:57 +0200

    Ajout d'un message dans l'exception CheckFileXMLException pour la classe XmlManagerTheLandOfSika Appel check_xml_file() lors de la création de l'instance XmlManagerTheLandOfSika

--- XmlManage/XmlManager_TheLandOfSika.py
+++ XmlManage/XmlManager_TheLandOfSika.py
@@ -1,4 +1,4 @@
-from Exceptions.XmlManage.Exceptions_XmlManage import CheckFileXML
+from Exceptions.XmlManage.Exceptions_XmlManage import CheckFileXMLException
 from XmlManage.XmlManager import XmlManager
 
 
@@ -7,7 +7,9 @@ class XmlManagerTheLandOfSika(XmlManager):
 
     def __init__(self, file_path):
         XmlManager.__init__(self, file_path)
+        self.check_xml_file()
+
 
     def check_xml_file(self):
         if not self.get_nodes("strings"):
-            raise CheckFileXML(file_path=self.file_path, manager_name=self.manager_name)
+            raise CheckFileXMLException(f"Le fichier xml {self.file_path} n'est pas reconnue par le manager {self.manager_name}", file_path=self.file_path, manager_name=self.manager_name)
