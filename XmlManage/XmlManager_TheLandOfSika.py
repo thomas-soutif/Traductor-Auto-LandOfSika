967eef7be11191022a624ff7432718e88b9e9a44
thomas_soutif <thomas.soutif@nuabee.fr>
2022-07-26 15:42:42 +0200

    Ajout du Manager XmlManagerTheLandOfSika avec l'implémentation du check_xml_file

---
+++ XmlManage/XmlManager_TheLandOfSika.py
@@ -0,0 +1,12 @@
+from Exceptions.XmlManage.Exceptions_XmlManage import CheckFileXML
+from XmlManage.XmlManager import XmlManager
+
+
+class XmlManagerTheLandOfSika(XmlManager):
+    manager_name = "TheLandOfSika"
+    def __init__(self,file_path):
+       XmlManager.__init__(self,file_path)
+
+    def check_xml_file(self):
+        if not self.get_nodes("strings"):
+            raise CheckFileXML(file_path=self.file_path, manager_name=self.manager_name)
