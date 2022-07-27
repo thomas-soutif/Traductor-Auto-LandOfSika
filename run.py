cdc26b0e1a2698e2cd171d6faa59cc2bd6f557fc
thomas_soutif <thomas.soutif@nuabee.fr>
2022-07-27 16:01:55 +0200

    Ajout de code dans run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import click
-import sys
 from source.XmlManager.TheLandOfSika import XmlManagerTheLandOfSika
 
 
@@ -9,7 +8,16 @@ def run(file_path: str):
     if not file_path:
         raise Exception("You must pass a xml file as first argument")
     xml_manager = XmlManagerTheLandOfSika(file_path=file_path)
+    for node in xml_manager.get_all_translate_nodes():
+        if not xml_manager.check_if_is_correct_translate_node(node):
+            continue
+        xml_manager.set_text_translate_node(node, "ceci est un test")
+
+    xml_manager.save_file(file_path.replace(".xml", "") + "COPY.xml")
+
 
 
 if __name__ == "__main__":
     run()
+
+
