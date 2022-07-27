078f96d6f144702c29158b97717a57d455408ab1
Soutif Thomas <th.soutif@gmail.com>
2022-07-27 21:32:31 +0200

    Les variables dynamiques sont désormais écrit dans un fichier

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from json import JSONDecodeError
 from source.FileManager.DynamicVariablesFileManager import DynamicVariablesFileManager
 import click
 
-from config import BASE_DIR
+from config import BASE_DIR, DYNAMIC_VARIABLES_FILE_PATH
 from source.XmlManager.TheLandOfSika import XmlManagerTheLandOfSika
 
 
@@ -18,7 +18,7 @@ def run(file_path: str):
     # We check first if there is some dynamic variable to setup
 
     dynamic_variables = xml_manager.get_list_dynamic_variable()
-    dynamic_variables_and_value_enter = None
+    dynamic_variables_and_value_enter = {}
     if dynamic_variables:
         logging.warning(
             "Some dynamic variables have been found in the XML file, please fill for each one a word that fit it correctly, to be able to translate the sentence. You can left it empty and the sentence associated will be ignored and not translated.")
@@ -28,9 +28,15 @@ def run(file_path: str):
             print(f"{variable} ==> ")
             value_enter_by_user = input()
             dynamic_variables_and_value_enter.update({variable: value_enter_by_user})
-        DynamicVariablesFileManager(file_path=BASE_DIR)
-    print(dynamic_variables_and_value_enter)
-    exit()
+
+        #We save to a file the values, to be used again
+        file_manager = DynamicVariablesFileManager(file_path=DYNAMIC_VARIABLES_FILE_PATH)
+        json_data = file_manager.get_json_data()
+        if not json_data:
+            json_data ={}
+        json_data.update(dynamic_variables_and_value_enter)
+        file_manager.write_json_data(json_data)
+
     for node in xml_manager.get_all_translate_nodes():
         if not xml_manager.check_if_is_correct_translate_node(node):
             continue
