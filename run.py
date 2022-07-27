1552e0a8e8474bcfb2fed3ac4ff6aff9844578e6
Soutif Thomas <th.soutif@gmail.com>
2022-07-27 21:55:03 +0200

    Les variables dynamiques peuvent désormais être réutilisé par l'utilisateur grâce au fichier de configuration qui les sauvegarde

--- run.py
+++ run.py
@@ -20,22 +20,37 @@ def run(file_path: str):
     dynamic_variables = xml_manager.get_list_dynamic_variable()
     dynamic_variables_and_value_enter = {}
     if dynamic_variables:
+        file_manager = DynamicVariablesFileManager(file_path=DYNAMIC_VARIABLES_FILE_PATH)
+        dynamic_variables_of_file = file_manager.get_json_data()
         logging.warning(
             "Some dynamic variables have been found in the XML file, please fill for each one a word that fit it correctly, to be able to translate the sentence. You can left it empty and the sentence associated will be ignored and not translated.")
-        print("\n")
-        dynamic_variables_and_value_enter: dict = {}
-        for variable in dynamic_variables:
-            print(f"{variable} ==> ")
-            value_enter_by_user = input()
-            dynamic_variables_and_value_enter.update({variable: value_enter_by_user})
 
-        #We save to a file the values, to be used again
-        file_manager = DynamicVariablesFileManager(file_path=DYNAMIC_VARIABLES_FILE_PATH)
-        json_data = file_manager.get_json_data()
-        if not json_data:
-            json_data ={}
-        json_data.update(dynamic_variables_and_value_enter)
-        file_manager.write_json_data(json_data)
+        if dynamic_variables_of_file:
+            use_config_data = False
+            print(
+                "A configuration file have been found for the dynamic variable, would you like to use it to automatically fill the translation ?(By Default No) [Yes, No]")
+            value_user = input()
+            if value_user.lower() == "yes":
+                use_config_data = True
+            elif value_user.lower() == "no":
+                use_config_data = False
+            else:
+                logging.info("We was waiting for Yes or No, use of the default value (No)")
+
+            if use_config_data:
+                for key in dynamic_variables:
+                    if dynamic_variables_of_file.get(key):
+                        dynamic_variables.remove(key)
+
+        if dynamic_variables:
+            dynamic_variables_and_value_enter: dict = user_input_dynamic_variable(dynamic_variables)
+
+        # We save to a file the values, to be used again
+
+        if not dynamic_variables_of_file:
+            dynamic_variables_of_file = {}
+        dynamic_variables_of_file.update(dynamic_variables_and_value_enter)
+        file_manager.write_json_data(dynamic_variables_of_file)
 
     for node in xml_manager.get_all_translate_nodes():
         if not xml_manager.check_if_is_correct_translate_node(node):
@@ -45,6 +60,14 @@ def run(file_path: str):
     xml_manager.save_file(file_path.replace(".xml", "") + "COPY.xml")
 
 
+def user_input_dynamic_variable(dynamic_variables) -> dict:
+    dynamic_variables_and_value_enter = {}
+    for variable in dynamic_variables:
+        print(f"{variable} ==> ")
+        value_enter_by_user = input()
+        dynamic_variables_and_value_enter.update({variable: value_enter_by_user})
+
+    return dynamic_variables_and_value_enter
 
 
 if __name__ == "__main__":
