ba38283d52c8a5bf4562eda1fd1083b2bc1d0068
Soutif Thomas <th.soutif@gmail.com>
2022-07-28 08:04:25 +0200

    Ajout documentation des fonctions dans l'API Translator DeepL

--- source/APITranslator/DeepL.py
+++ source/APITranslator/DeepL.py
@@ -16,6 +16,10 @@ class DeepLTranslator(APITranslator):
         self.check_auth()
 
     def auth(self):
+        """
+        Load the KEY_DEEPL_API in the .env file to authentificate you on the DeepL API.
+        :return:
+        """
         load_dotenv()
         if not (os.getenv("KEY_DEEPL_API")):
             raise DeepLAuthKeyNotSetException(
@@ -23,10 +27,21 @@ class DeepLTranslator(APITranslator):
         self.translator_object = deepl.Translator(os.getenv("KEY_DEEPL_API"))
 
     def translate(self, text_to_translate: str, source_language: str, target_language: str):
+        """
+        Translate a text with DeepL to the specified target language.
+        :param text_to_translate:
+        :param source_language:
+        :param target_language:
+        :return: The text translated
+        """
         result = self.translator_object.translate_text(text_to_translate, target_lang=target_language)
         return result.text
 
     def check_auth(self):
+        """
+        Check if the authentification for the DeepL API is correct. For more information on how to setup it, visit the DeepL Api Website
+        :return: An exeception if the check failed
+        """
         try:
             self.translator_object.get_usage()
         except deepl.exceptions.AuthorizationException:
@@ -35,4 +50,9 @@ class DeepLTranslator(APITranslator):
             raise e
 
     def translate_to_french(self, text_to_translate):
+        """
+        Translate a text with DeepL to the French language
+        :param text_to_translate:
+        :return:
+        """
         return self.translate(text_to_translate=text_to_translate, source_language="", target_language="FR")
