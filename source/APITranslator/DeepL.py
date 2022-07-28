b8abb183fccd2ea19fd74b1348a836386714767b
Soutif Thomas <th.soutif@gmail.com>
2022-07-28 08:26:40 +0200

    amélio de la doc

--- source/APITranslator/DeepL.py
+++ source/APITranslator/DeepL.py
@@ -12,7 +12,8 @@ from ..Exceptions import DeepLAuthKeyNotSetException, DeepLAuthKeyWrongException
 class DeepLTranslator(APITranslator):
     translator_object = None
     target_language_full = None
-    def __init__(self,target_language_full : str):
+
+    def __init__(self, target_language_full: str):
         self.target_language_full = target_language_full
         self.auth()
         self.check_auth()
@@ -61,30 +62,30 @@ class DeepLTranslator(APITranslator):
         """
         return self.translate(text_to_translate=text_to_translate, source_language="", target_language="FR")
 
-    def managed_languages(self):
+    def managed_languages(self) -> dict:
         """
         Return a list of the languages managed by the module for DeepL. It can be configure in the config.py file
         :return: A dict that contains the language and his target_language named from the DeepL API point of view
         """
         return managed_languages_DEEPL
 
-    def check_if_language_managed(self):
+    def check_if_language_managed(self) -> None:
         """
         Check if the language target name (for example French) is managed by the module
         :param language_target_name:
-        :return:
+        :return: raise of the language is not supported by the module, else None
         """
         if not self.managed_languages().get(self.target_language_full, None):
-            raise DeepLLanguageTargetNotManagedException(f"The language {self.target_language_full} is not managed by the "
-                                                         f"DeepL Module. Please add it to the configuration file "
-                                                         f"'config.py' in the variable 'managed_languages_DEEPL'. The list of language managed is {list(self.managed_languages().keys())}")
+            raise DeepLLanguageTargetNotManagedException(
+                f"The language {self.target_language_full} is not managed by the "
+                f"DeepL Module. Please add it to the configuration file "
+                f"'config.py' in the variable 'managed_languages_DEEPL'. The list of language managed is {list(self.managed_languages().keys())}")
 
-
-    def get_target_language_key(self):
+    def get_target_language_key(self) -> str:
         """
         Get the key name of the target language key managed by the module. For example, the key of "French" is "FR"
         :param language_target_name:
-        :return:
+        :return: The key
         """
         self.check_if_language_managed()
         return self.managed_languages().get(self.target_language_full)
